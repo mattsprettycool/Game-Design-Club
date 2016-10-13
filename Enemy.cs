1a72e2961f60c2df57459cb40f9688d980e6bc59
jaisaka <jaitheboss1@gmail.com>
2016-10-12 20:45:48 -0400

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public Rigidbody2D rb;
+	public float maxSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float xpose = gameObject.transform.position.x;
+		float ypose = gameObject.transform.position.y;
+		StartCoroutine (timeandmove());
+		gameObject.transform.position = new Vector2(xpose, ypose);
+	}
+	IEnumerator timeandmove(){
+		float speed = 10f;
+		rb.AddForce (transform.right * speed);
+		yield return new WaitForSeconds(1);
+		rb.AddForce ((-1*transform.right)*speed);
+	}
+}
