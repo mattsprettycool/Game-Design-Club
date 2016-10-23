f109725407dd1bffc6840679031073d178025acd
jaisaka <jaitheboss1@gmail.com>
2016-10-23 16:37:20 -0400

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,26 +1,12 @@
-﻿using UnityEngine;
-using System.Collections;
-//By:Jai Saka
+//by jaisaka
+using UnityEngine;
 public class Enemy : MonoBehaviour {
-	public Rigidbody2D rb;
-	public float maxSpeed;
+	public float speed = 2f; //gotta go fast
+	public float length = 8f; //length to go
+	public float posX = 0f; //x pos
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
-	}
-
-	// Update is called once per frame
-	void Update () {
-		float xpose = gameObject.transform.position.x;
-		float ypose = gameObject.transform.position.y;
-		StartCoroutine (timeandmove());
-		gameObject.transform.position = new Vector2(xpose, ypose);
-	}
-	IEnumerator timeandmove(){
-		float speed = 10f;
-		rb.AddForce (transform.right * speed);
-		yield return new WaitForSeconds(1);
-		rb.AddForce ((-1*transform.right)*speed);
+	void Update() {
+		Vector2 pos = new Vector2 ( posX+Mathf.PingPong(speed * Time.time, length),0);
+		transform.position = pos;
 	}
 }
