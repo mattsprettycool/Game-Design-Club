ba6b3deb2776ed9c34b7f1b0b4dd3bc978bb88db
mattsprettycool <matt.braden73@gmail.com>
2016-10-24 12:20:06 -0400

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 	public float speed = 2f; //gotta go fast
 	public float length = 8f; //length to go
-	public float posX = 0f; //x pos
+	public float posX; //x pos
+	public float posY;
+	public Rigidbody2D thes;
+
+	void Start(){
+		posX = gameObject.transform.position.x;
+		posY = gameObject.transform.position.y;
+	}
 
 	void Update() {
-		Vector2 pos = new Vector2 ( posX+Mathf.PingPong(speed * Time.time, length),0);
-		transform.position = pos;
+		Vector2 pos = new Vector2 ( posX+Mathf.PingPong(speed * Time.time, length),posY);
+		thes.AddForce(pos);
 	}
 }
